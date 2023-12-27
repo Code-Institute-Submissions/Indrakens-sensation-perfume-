5b0caeaaf693407422ad974055f6d2b84bb40869
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-12-27 17:05:04 +0000

    Added price was to products

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     description = models.TextField()
     include = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2) 
+    price_was = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) 
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True) 
     image = models.ImageField(null=True, blank=True) 
