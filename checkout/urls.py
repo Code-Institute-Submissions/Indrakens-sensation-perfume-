f74ef0191bfe5110237e09abd71b1bb89dd67562
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-01-07 22:44:18 +0000

    Started order checkout succsess

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.order_checkout, name='order_checkout')
+    path('', views.order_checkout, name='order_checkout'),
+    path('order_checkout_success/<order_number>', views.order_checkout_success, name='order_checkout_success'),
 ] 
