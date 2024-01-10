f7518db061ea359eb331a49a8717a255b7331c63
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-01-10 13:24:17 +0000

    Updated country in models

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,7 +24,7 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2', 
             'town_or_city': 'Town or City', 
             'county': 'County, State or Locality', 
-            'postcode': 'Postal Code',
+            'postcode': 'Postal Code', 
         } 
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
