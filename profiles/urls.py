a6a7263db3309f6f7502221b3ad16e52e764956b
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-01-16 00:20:46 +0000

    Added order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path("", views.profile, name="profile"),
+    path(
+        "user_order_history/<order_number>",
+        views.user_order_history,
+        name="user_order_history",
+    ),
 ] 
