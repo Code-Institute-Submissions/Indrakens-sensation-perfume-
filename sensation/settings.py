31fa4dc4747c8640e7f50379c797baef1a0973e2
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-01-21 15:17:19 +0000

    Removed secret kay and set debug

--- sensation/settings.py
+++ sensation/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$p*xv=4m+^ku2xmjxt8nz@+7a7^g!i3nv)f$_4t$u5rc^bh5*('
+SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = 'DEVELOPMENT' in os.environ 
 
 ALLOWED_HOSTS = ['sensation-perfume-812e78510f66.herokuapp.com', 'localhost']  
 
