a47ca61427c8cce49cada7246c2e81a63ec4e480
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-01-30 23:12:35 +0000

    Added privacy policy and sitemap

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     A view to return the index page 
     """
     return render(request, 'home/index.html') 
+
+
+def privacy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy_policy.html')    
