49d64bca1aa19b13c99a9bcc06dd45eda6762805
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-04 14:54:48 +0000

    Added comment form in blog

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect 
 from .models import Post
 from profiles.models import UserProfile
+from .forms import CommentForm 
 
 
 class PostList(generic.ListView):
@@ -16,6 +17,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-created_on") 
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -25,25 +27,45 @@ class PostDetail(View):
             "blog/blog_detail.html",
             {
                 "post": post,
+                "comments": comments,
                 "liked": liked,
+                "comment_form": CommentForm() 
             },
         )
    
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-created_on") 
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.username = request.user
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.success(request,
+                             'Your comment has been posted')
+        else:
+            comment_form = CommentForm()
+            messages.warning(request,
+                             'Something went wrong. Please Try Again')
+    
 
         return render(
             request,
             "blog/blog_detail.html",
             {
                 "post": post,
-                "liked": liked
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm(),
             },
-        )
+        ) 
 
 
 class PostLike(View):
