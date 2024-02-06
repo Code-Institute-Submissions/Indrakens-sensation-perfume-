a4cdbba69b4137f05387477f029d5945eebbb055
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-06 20:59:10 +0000

    Updated blog.html template

--- blog/models.py
+++ blog/models.py
@@ -43,4 +43,4 @@ class Comment(models.Model):
         ordering = ["created_on"] 
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name.profile.profile_picture}"  
+        return f"Comment {self.body} by {self.name}"  
