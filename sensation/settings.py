7c1915d71d4ce967e9f0460c134cada49a0cc6e7
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-17 17:48:29 +0000

    Added cancel link in update user profile template

--- sensation/settings.py
+++ sensation/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent 
 
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ   
+DEBUG = 'DEVELOPMENT' in os.environ  
 
 
 ALLOWED_HOSTS = [
