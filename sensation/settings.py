78324a0faf73185ed9db4c524b879d98105eff2b
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-18 10:29:26 +0000

    Deployment

--- sensation/settings.py
+++ sensation/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os 
 import dj_database_url
 from pathlib import Path 
+if os.path.isfile('env.py'):
+    import env
+
 
 
 
@@ -24,14 +27,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '') 
+#SECRET_KEY = os.environ.get('SECRET_KEY', '') 
+SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ  
+#DEBUG = 'DEVELOPMENT' in os.environ 
+DEBUG = True  
 
 
 ALLOWED_HOSTS = [
-    'sensation-perfume-812e78510f66.herokuapp.com', 
+    'sensation-perfume.herokuapp.com', 
     'localhost'
     ]  
 
@@ -129,7 +134,7 @@ WSGI_APPLICATION = 'sensation.wsgi.application'
 
 #if 'DATABASE_URL' in os.environ:
 DATABASES = {
-    'default': dj_database_url.parse('postgres://vientlwx:PjsRPbK4SNIXzdPSFj8VEcqxMKutf024@horton.db.elephantsql.com/vientlwx') 
+    'default': dj_database_url.parse('postgres://xjmystub:xrKLfPMVYk1EEPz3zq3QTjoAvZ5v7GUK@trumpet.db.elephantsql.com/xjmystub') 
 }
 #else:    
 #    DATABASES = {
@@ -182,29 +187,29 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
-     # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    } 
+#if 'USE_AWS' in os.environ:
+#     # Cache control
+#    AWS_S3_OBJECT_PARAMETERS = {
+#        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#        'CacheControl': 'max-age=94608000',
+#    } 
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'sensation-perfume'
-    AWS_S3_REGION_NAME = 'eu-west-1' 
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#    AWS_STORAGE_BUCKET_NAME = 'sensation-perfume'
+#    AWS_S3_REGION_NAME = 'eu-west-1' 
+#    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
+#    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media' 
+#    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+#    STATICFILES_LOCATION = 'static'
+#    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+#    MEDIAFILES_LOCATION = 'media' 
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/' 
+#    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+#    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/' 
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 85 
@@ -214,18 +219,19 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '') 
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '') 
 
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'sensationperfume24@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')      
+#if 'DEVELOPMENT' in os.environ:
+#    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#    DEFAULT_FROM_EMAIL = 'sensationperfume24@example.com'
+#else:
+#    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#    EMAIL_USE_TLS = True
+#    EMAIL_PORT = 587
+#    EMAIL_HOST = 'smtp.gmail.com'
+#    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+#    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+#    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')      
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
