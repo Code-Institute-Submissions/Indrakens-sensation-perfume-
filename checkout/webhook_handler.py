d534497a59561bec679502136c7bd1451cadd00b
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-21 14:04:05 +0000

    Edited webhook

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,7 +20,7 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        cust_email = order.email
+        cust_email = order.user_email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
