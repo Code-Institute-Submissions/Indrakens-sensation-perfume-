b9e6d1ce63f1e47e228d7ee96d3f4d82a03d5213
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-02-21 11:12:47 +0000

    Edited stripe element template

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -61,8 +61,7 @@ form.addEventListener('submit', function(ev) {
             payment_method: {
                 card: card,
                 billing_details: { 
-                    first_name: $.trim(form.first_name.value), 
-                    last_name: $.trim(form.last_name.value),
+                    name: $.trim(form.full_name.value), 
                     phone: $.trim(form.user_phone_number.value),
                     email: $.trim(form.user_email.value),
                     address:{
@@ -75,8 +74,7 @@ form.addEventListener('submit', function(ev) {
                 }
             },
             shipping: {
-                first_name: $.trim(form.first_name.value),
-                last_name: $.trim(form.last_name.value), 
+                name: $.trim(form.full_name.value),
                 phone: $.trim(form.user_phone_number.value),
                 address: {
                     line1: $.trim(form.user_street_address1.value),
