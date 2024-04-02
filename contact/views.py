2b278f93d9ed77bfc51b83add6abd4d991b32c61
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-04-02 21:38:11 +0000

    Updated toast success template and contact us template

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import ContactForm
 from django.contrib import messages
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.conf import settings
 
 
 def contact_us(request):
@@ -17,21 +14,8 @@ def contact_us(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            subject = render_to_string(
-                'contact/confirmation_emails/email_subject.txt',
-                )
-            body = render_to_string(
-                'contact/confirmation_emails/email_body.txt',
-                )
-        
-            send_mail(
-                form,
-                subject,
-                body,
-                settings.DEFAULT_FROM_EMAIL,
-            )                
-            messages.success(request, 'Your message sent successfuly!')
+            form.save()                
+            messages.success(request, 'Message sent succesfuly. Thank you for contacting with us!')
             return redirect(reverse('home'))
         else:
             messages.error(request, 'Something went wrong, please try again.')
