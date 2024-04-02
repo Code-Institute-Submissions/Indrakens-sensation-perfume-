0e3a0ff8379349ed439c48e08599c227f7b28159
Indrakens Zilinska <indrazilinska83@gmail.com>
2024-04-02 22:11:22 +0000

    Edited free delivery banner color

--- sensation/settings.py
+++ sensation/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent 
 
@@ -34,7 +36,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     'sensation-perfume-d52586ead80b.herokuapp.com', 
     'localhost'
-]  
+]
 
 
 # Application definition
